4a23b2faa816fa847542ab97cf7df54abdb3b956
haby <haby.romaric@gmail.com>
2022-05-03 20:52:58 +0200

    refont ui

--- app/src/main/java/com/formasdis/ui/fragment/RecruitmentFragment.kt
+++ app/src/main/java/com/formasdis/ui/fragment/RecruitmentFragment.kt
@@ -3,10 +3,13 @@ package com.formasdis.ui.fragment
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.RoundedCorner
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.TextView
+import coil.load
+import coil.transform.RoundedCornersTransformation
 import com.formasdis.R
 import com.formasdis.ui.fragment.recruitment.ProfessionalFragment
 import com.formasdis.ui.fragment.recruitment.VolunteerFragment
@@ -19,49 +22,64 @@ class RecruitmentFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        var view = inflater.inflate(R.layout.fragment_recruitment, container, false)
+        // View
+        val view = inflater.inflate(R.layout.fragment_recruitment, container, false)
+
+        // Get item in view
         val toolBarTitle = view.findViewById<TextView>(R.id.titleToolBar)
         val toolBarBack = view.findViewById<ImageButton>(R.id.imageButtonBack)
         val imageYoung = view.findViewById<ImageButton>(R.id.imageButtonYoung)
         val imageProfessional = view.findViewById<ImageButton>(R.id.imageButtonProfessional)
         val imageVolunteer = view.findViewById<ImageButton>(R.id.imageButtonVolunteer)
 
+
+        // ToolBar
         toolBarBack.visibility= View.VISIBLE
         toolBarTitle.visibility = View.VISIBLE
         toolBarTitle.text = "Devenir sapeur-pompier"
         toolBarTitle.textSize = 20F
 
-        imageYoung.setOnClickListener {
-            loadFragment(YoungFragment())
+        toolBarBack.setOnClickListener {
+            loadFragment(HomeFragment())
         }
 
-        imageProfessional.setOnClickListener {
-            loadFragment(ProfessionalFragment())
+        // Volunteer
+        imageVolunteer.load("https://demarchesadministratives.fr/images/demarches/957/comment-devenir-pompier-volontaire-1.jpg") {
+            crossfade(true)
+            placeholder(R.drawable.ic_launcher_foreground)
+            transformations(RoundedCornersTransformation(20f,20f,20f,20f))
+            size(810,465)
         }
 
         imageVolunteer.setOnClickListener {
             loadFragment((VolunteerFragment()))
         }
 
-        Picasso.get()
-            .load("https://remeng.rosselcdn.net/sites/default/files/dpistyles_v2/ena_16_9_extra_big/2021/07/20/node_277083/12347467/public/2021/07/20/B9727751521Z.1_20210720110621_000%2BGN4IIQBQT.1-0.jpg?itok=i70GaQBG1626771987")
-            .resize(imageYoung.width,450)
-            .into(imageYoung)
 
-        Picasso.get()
-            .load("https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Pompiers_IMG_2761.JPG/520px-Pompiers_IMG_2761.JPG")
-            .resize(imageProfessional.width,450)
-            .into(imageProfessional)
-
-        Picasso.get()
-            .load("https://demarchesadministratives.fr/images/demarches/957/comment-devenir-pompier-volontaire-1.jpg")
-            .resize(imageVolunteer.width,450)
-            .into(imageVolunteer)
-
-        toolBarBack.setOnClickListener {
-            loadFragment(HomeFragment())
+        // Professional
+        imageProfessional.load("https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Pompiers_IMG_2761.JPG/520px-Pompiers_IMG_2761.JPG") {
+            crossfade(true)
+            placeholder(R.drawable.ic_launcher_foreground)
+            transformations(RoundedCornersTransformation(20f,20f,20f,20f))
+            size(810,465)
         }
+
+        imageProfessional.setOnClickListener {
+            loadFragment(ProfessionalFragment())
+        }
+
+        // Young
+        imageYoung.load("https://remeng.rosselcdn.net/sites/default/files/dpistyles_v2/ena_16_9_extra_big/2021/07/20/node_277083/12347467/public/2021/07/20/B9727751521Z.1_20210720110621_000%2BGN4IIQBQT.1-0.jpg?itok=i70GaQBG1626771987") {
+            crossfade(true)
+            placeholder(R.drawable.ic_launcher_foreground)
+            transformations(RoundedCornersTransformation(20f,20f,20f,20f))
+            size(810,465)
+        }
+
+        imageYoung.setOnClickListener {
+            loadFragment(YoungFragment())
+        }
+
         return view
     }
 
