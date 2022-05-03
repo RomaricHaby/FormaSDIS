ca6ff1cc934ff5da7c05791c56dd59b436dde718
roro <dev1@cap-on-line.net>
2022-05-03 15:17:26 +0200

    add quiz function

--- app/src/main/java/com/formasdis/network/DataQuiz.kt
+++ app/src/main/java/com/formasdis/network/DataQuiz.kt
@@ -7,6 +7,9 @@ import com.formasdis.model.Quiz
 object DataQuiz {
     val listQuiz = ArrayList<Quiz>()
 
+    fun addQuiz(quiz: Quiz) {
+        ClientFirebase.myRef.child("quiz").child(quiz.id.toString()).setValue(quiz)
+    }
 
     fun getAllQuiz() {
         ClientFirebase.myRef.child("quiz").get().addOnSuccessListener {
@@ -22,7 +25,7 @@ object DataQuiz {
                         "name" -> name = dataQuiz.value.toString()
                         "nbrQuestion" -> nbrQuestion = dataQuiz.value.toString().toInt()
                         "type" -> type = dataQuiz.value.toString()
-                        "questions" -> {
+                        "listQuestions" -> {
                             for (questionsId in dataQuiz.children) {
                                 var typeQuestion = 0
                                 var question = ""
@@ -30,17 +33,20 @@ object DataQuiz {
 
                                 for (dataQuestion in questionsId.children) {
                                     when (dataQuestion.key.toString()) {
-                                        "type" -> typeQuestion = dataQuestion.value.toString().toInt()
-                                        "question" -> question = dataQuestion.value.toString()
-                                        "answer" -> {
+                                        "type" -> typeQuestion =
+                                            dataQuestion.value.toString().toInt()
+                                        "nameQuestion" -> question = dataQuestion.value.toString()
+                                        "listAnswer" -> {
                                             for (answerId in dataQuestion.children) {
                                                 var answer = ""
                                                 var correct = false
 
                                                 for (dataAnswer in answerId.children) {
                                                     when (dataAnswer.key.toString()) {
-                                                        "answer" -> answer = dataAnswer.value.toString()
-                                                        "correct" -> correct = dataAnswer.value.toString().toBoolean()
+                                                        "answer" -> answer =
+                                                            dataAnswer.value.toString()
+                                                        "correct" -> correct =
+                                                            dataAnswer.value.toString().toBoolean()
                                                     }
                                                 }
                                                 listAnswer.add(Answer(answer, correct))
@@ -55,6 +61,56 @@ object DataQuiz {
                 }
                 listQuiz.add(Quiz(id, name, nbrQuestion, type, listQuestions))
             }
+        }
+    }
+
+    fun getQuizById(id: Int) {
+        ClientFirebase.myRef.child("quiz").child(id.toString()).get().addOnSuccessListener {
+            var name = "null"
+            var nbrQuestion = 0
+            var type = ""
+            val listQuestions = ArrayList<Question>()
+
+            for (dataQuiz in it.children) {
+                when (dataQuiz.key.toString()) {
+                    "name" -> name = dataQuiz.value.toString()
+                    "nbrQuestion" -> nbrQuestion = dataQuiz.value.toString().toInt()
+                    "type" -> type = dataQuiz.value.toString()
+                    "listQuestions" -> {
+                        for (questionsId in dataQuiz.children) {
+                            var typeQuestion = 0
+                            var question = ""
+                            val listAnswer = ArrayList<Answer>()
+
+                            for (dataQuestion in questionsId.children) {
+                                when (dataQuestion.key.toString()) {
+                                    "type" -> typeQuestion =
+                                        dataQuestion.value.toString().toInt()
+                                    "nameQuestion" -> question = dataQuestion.value.toString()
+                                    "listAnswer" -> {
+                                        for (answerId in dataQuestion.children) {
+                                            var answer = ""
+                                            var correct = false
+
+                                            for (dataAnswer in answerId.children) {
+                                                when (dataAnswer.key.toString()) {
+                                                    "answer" -> answer =
+                                                        dataAnswer.value.toString()
+                                                    "correct" -> correct =
+                                                        dataAnswer.value.toString().toBoolean()
+                                                }
+                                            }
+                                            listAnswer.add(Answer(answer, correct))
+                                        }
+                                    }
+                                }
+                            }
+                            listQuestions.add(Question(question, typeQuestion, listAnswer))
+                        }
+                    }
+                }
+            }
+            //  listQuiz.add(Quiz(id, name, nbrQuestion, type, listQuestions))
         }
     }
 }
