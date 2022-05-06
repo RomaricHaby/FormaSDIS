eb36b1ad4b50d966f886954eb51986175f58026f
DimitriC71 <92568593+DimitriC71@users.noreply.github.com>
2022-05-06 09:40:55 +0200

    page formation

--- app/src/main/java/com/formasdis/ui/fragment/education/WebViewFragment.kt
+++ app/src/main/java/com/formasdis/ui/fragment/education/WebViewFragment.kt
@@ -1,6 +1,8 @@
 package com.formasdis.ui.fragment.education
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,6 +16,7 @@ import com.formasdis.R
 
 
 class WebViewFragment(private val urlPdf: String, private val isPdf: Boolean) : Fragment() {
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,21 +24,20 @@ class WebViewFragment(private val urlPdf: String, private val isPdf: Boolean) : 
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_pdf_reader, container, false)
         val webView: WebView = view.findViewById(R.id.webviewPdf)
-        webView.webViewClient = WebViewClient()
+        //webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
 
         if(!isPdf) {
             webView.settings.setSupportZoom(true)
-
             webView.settings.javaScriptEnabled = true
             webView.loadUrl(urlPdf)
         }
         else {
             webView.settings.setSupportZoom(true)
-
             webView.settings.javaScriptEnabled = true
-
             //webView.getSettings().setJavaScriptEnabled(true)
             webView.loadUrl("https://docs.google.com/gview?embedded=true&url=$urlPdf")
+
 
             //val settings: WebSettings = webView.getSettings()
             //settings.javaScriptEnabled = true
--- app/src/main/java/com/formasdis/ui/fragment/recruitment/YoungFragment.kt
+++ app/src/main/java/com/formasdis/ui/fragment/recruitment/YoungFragment.kt
@@ -7,6 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import coil.load
+import coil.transform.RoundedCornersTransformation
 import com.formasdis.R
 
 class YoungFragment : Fragment() {
@@ -16,6 +21,23 @@ class YoungFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_young, container, false)
+
+        val toolBarTitle = view.findViewById<TextView>(R.id.titleToolBar)
+        val toolBarBack = view.findViewById<ImageButton>(R.id.imageButtonBack)
+
+        val image = view.findViewById<ImageView>(R.id.imageViewYoung)
+
+        // ToolBar
+        toolBarBack.visibility= View.VISIBLE
+        toolBarTitle.visibility = View.VISIBLE
+        toolBarTitle.text = "DEVENIR JEUNE SAPEUR-POMPIER"
+        toolBarTitle.textSize = 12F
+
+        image.load("https://www.pompiers.fr/sites/default/files/content/text/picture/jsp.jpg") {
+            crossfade(true)
+            placeholder(R.drawable.ic_launcher_foreground)
+            transformations(RoundedCornersTransformation(20f,20f,20f,20f))
+        }
 
         return view
     }
