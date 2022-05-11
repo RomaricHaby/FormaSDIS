942201fe15d52b13eb2d43ba6ab002127c1d1dbc
DimitriC71 <92568593+DimitriC71@users.noreply.github.com>
2022-05-11 12:09:09 +0200

    list educ

--- app/src/main/java/com/formasdis/network/DataLesson.kt
+++ app/src/main/java/com/formasdis/network/DataLesson.kt
@@ -24,6 +24,116 @@ object DataLesson {
             "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449301",
             "Le rôle des binômes"
         ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/389705",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/964385",
+            "Les itinéraires et l'identification des faces"
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/389711",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/921029",
+            "Le zonage opérationnel"
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/389713",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449307",
+            "Explosimètrie"
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/389717",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449309",
+            "Les points d'eau incendie"
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/389719",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449311",
+            "Matériel incendie "
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/389739",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449315",
+            "Lot de Sauvetage et de Protection Contre les Chutes (L.S.P.C.C)"
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/389741",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449313",
+            "Les échelles à mains"
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/389743",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449317",
+            "Le travail en hauteur "
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/389745",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449319",
+            "Vérification du L.S.P.C.C, lot échelle et lots Maintient au Poste de Travail, Engin Spécifique, Engin Aérien et Roll Anti-Chute"
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/389747",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449321",
+            "Les manœuvres de base"
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/389763",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449323",
+            "Le tuyau en O "
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/389769",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449325",
+            "Sécurisation des intervenants sur les voies ouvertes à la circulation (SIVOC ROUTE)"
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/389771",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449267",
+            "Les phénomènes thermiques "
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/389811",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449269",
+            "Les manœuvres combinées"
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/389833",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449273",
+            "Les feux de forêts "
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/389865",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449279",
+            "Sécurisation des intervenants sur les voies ouvertes à la circulation (SIVOC Autoroute)"
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/389867",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449281",
+            "Les reconnaissances"
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/903911",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/903907",
+            "Technique opérationnelle Établissement d'une Division d'Alimentation (D.AL.)"
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/389875",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449285",
+            "Techniques de sauvetage"
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/389875",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449287",
+            "Interventions gaz"
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/959161",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/959159",
+            "Le rideau stoppeur de fumées"
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/966447",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/966449",
+            "Incendier dans un parc de stationnement couvert (P.S.C.)"
+        ),
     )
 
     val listVariousOperation = arrayListOf(
