b59149cb2957160510a7a6daa894a4e73f0211d2
DimitriC71 <92568593+DimitriC71@users.noreply.github.com>
2022-05-11 14:53:12 +0200

    update pdf

--- app/src/main/java/com/formasdis/network/DataLesson.kt
+++ app/src/main/java/com/formasdis/network/DataLesson.kt
@@ -47,7 +47,7 @@ object DataLesson {
         Lesson(
             "https://enasis.univ-lyon1.fr/file/resource/media/389719",
             "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449311",
-            "Matériel incendie "
+            "Matériel incendie"
         ),
         Lesson(
             "https://enasis.univ-lyon1.fr/file/resource/media/389739",
@@ -62,12 +62,12 @@ object DataLesson {
         Lesson(
             "https://enasis.univ-lyon1.fr/file/resource/media/389743",
             "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449317",
-            "Le travail en hauteur "
+            "Le travail en hauteur"
         ),
         Lesson(
             "https://enasis.univ-lyon1.fr/file/resource/media/389745",
             "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449319",
-            "Vérification du L.S.P.C.C, lot échelle et lots Maintient au Poste de Travail, Engin Spécifique, Engin Aérien et Roll Anti-Chute"
+            "Vérification du L.S.P.C.C, lot échelle et lot Maintient au Poste de Travail, Engin Spécifique, Engin Aérien et Roll Anti-Chute"
         ),
         Lesson(
             "https://enasis.univ-lyon1.fr/file/resource/media/389747",
@@ -77,7 +77,7 @@ object DataLesson {
         Lesson(
             "https://enasis.univ-lyon1.fr/file/resource/media/389763",
             "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449323",
-            "Le tuyau en O "
+            "Le tuyau en O"
         ),
         Lesson(
             "https://enasis.univ-lyon1.fr/file/resource/media/389769",
@@ -87,7 +87,7 @@ object DataLesson {
         Lesson(
             "https://enasis.univ-lyon1.fr/file/resource/media/389771",
             "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449267",
-            "Les phénomènes thermiques "
+            "Les phénomènes thermiques"
         ),
         Lesson(
             "https://enasis.univ-lyon1.fr/file/resource/media/389811",
@@ -97,7 +97,7 @@ object DataLesson {
         Lesson(
             "https://enasis.univ-lyon1.fr/file/resource/media/389833",
             "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449273",
-            "Les feux de forêts "
+            "Les feux de forêts"
         ),
         Lesson(
             "https://enasis.univ-lyon1.fr/file/resource/media/389865",
@@ -120,7 +120,7 @@ object DataLesson {
             "Techniques de sauvetage"
         ),
         Lesson(
-            "https://enasis.univ-lyon1.fr/file/resource/media/389875",
+            "https://enasis.univ-lyon1.fr/file/resource/media/389877",
             "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449287",
             "Interventions gaz"
         ),
@@ -143,19 +143,75 @@ object DataLesson {
             "Les échelles à mains"
         ),
         Lesson(
-            "https://enasis.univ-lyon1.fr/file/resource/media/398559",
-            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/964383",
-            "LA PROTECTION DES INTERVENANTS LORS DES INTERVENTIONS POUR INCENDIE"
+            "https://enasis.univ-lyon1.fr/file/resource/media/389739",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449315",
+            "Lot de Sauvetage et de Protection Contre les Chutes (L.S.P.C.C)"
         ),
         Lesson(
-            "https://enasis.univ-lyon1.fr/file/resource/media/389699",
-            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449299",
-            "La MGO"
+            "https://enasis.univ-lyon1.fr/file/resource/media/389743",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449317",
+            "Le travail en hauteur"
         ),
         Lesson(
-            "https://enasis.univ-lyon1.fr/file/resource/media/389703",
-            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449301",
-            "Rôle des binômes"
+            "https://enasis.univ-lyon1.fr/file/resource/media/389745",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449319",
+            "Vérification du L.S.P.C.C, lot échelle et lot Maintient au Poste de Travail, Engin Spécifique, Engin Aérien et Roll Anti-Chute"
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/391623",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/453519",
+            "Matériel de forcement"
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/391619",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/453521",
+            "Les moyens d'éclairage"
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/391609",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/453503",
+            "Les tronçonneuses"
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/391617",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/453505",
+            "Le bâchage"
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/391621",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/453507",
+            "Étaiements"
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/391639",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/453509",
+            "Épuisement et assèchement"
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/391613",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/453511",
+            "Capture et sauvetage d’animaux"
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/391611",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/453513",
+            "Les hyménoptères"
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/391257",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/453515",
+            "Les panneaux solaires"
+        ),
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/391615",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/453517",
+            "Dégagement de personne d'une cabine d’ascenseur"
+        ),
+
+        Lesson(
+            "https://enasis.univ-lyon1.fr/file/resource/media/389769",
+            "https://enasis.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/449325",
+            "Sécurisation des intervenants sur les voies ouvertes à la circulation (SIVOC ROUTE)"
         ),
     )
 }
